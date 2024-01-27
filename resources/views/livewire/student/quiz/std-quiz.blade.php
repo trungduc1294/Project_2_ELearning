fecd223723bcc926ca4559adf62a87262fb0de79
trungduc1294 <trungduc.1294@gmail.com>
2024-01-27 18:44:54 +0700

    backup

--- resources/views/livewire/student/quiz/std-quiz.blade.php
+++ resources/views/livewire/student/quiz/std-quiz.blade.php
@@ -1,52 +1,123 @@
 <div class="quiz-page">
     <div class="back">
-        Back to lesson
+        <i class="fa-solid fa-arrow-left"></i>
+        <a href="{{route('student.lesson.detail', [
+            'course_id' => $course_id,
+            'lesson_id' => $lesson_id
+        ])}}">Back to lesson</a>
     </div>
 
     <div class="quiz-container">
-        <div class="quiz-block">
-            <div class="question">
-                <div class="question-title">
-                    <h3>Question 1</h3>
-                </div>
-                <div class="question-content">
-                    <p>What is the capital of the United States of America?</p>
-                </div>
+
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                @if ($quizList)
+                    @foreach ($quizList as $quiz)
+                        <div class="swiper-slide">
+                            <div class="quiz-block">
+                                <div class="question">
+                                    <div class="question-title">
+                                        <h3>Question {{$loop->index + 1}}:</h3>
+                                    </div>
+                                    <div class="question-content">
+                                        <p>{{$quiz->question}}</p>
+                                    </div>
+                                </div>
+                                <div class="answers">
+                                    <div class="answer">
+                                        <div class="answer-title">
+                                            <h3>A</h3>
+                                        </div>
+                                        <div class="answer-content">
+                                            <p>{{$quiz->answer_a}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="answer">
+                                        <div class="answer-title">
+                                            <h3>B</h3>
+                                        </div>
+                                        <div class="answer-content">
+                                            <p>{{$quiz->answer_b}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="answer">
+                                        <div class="answer-title">
+                                            <h3>C</h3>
+                                        </div>
+                                        <div class="answer-content">
+                                            <p>{{$quiz->answer_c}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="answer">
+                                        <div class="answer-title">
+                                            <h3>D</h3>
+                                        </div>
+                                        <div class="answer-content">
+                                            <p>{{$quiz->answer_d}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
-            <div class="answers">
-                <div class="answer">
-                    <div class="answer-title">
-                        <h3>Answer 1</h3>
-                    </div>
-                    <div class="answer-content">
-                        <p>Washington D.C.</p>
-                    </div>
-                </div>
-                <div class="answer">
-                    <div class="answer-title">
-                        <h3>Answer 2</h3>
-                    </div>
-                    <div class="answer-content">
-                        <p>Washington D.C.</p>
-                    </div>
-                </div>
-                <div class="answer">
-                    <div class="answer-title">
-                        <h3>Answer 3</h3>
-                    </div>
-                    <div class="answer-content">
-                        <p>Washington D.C.</p>
-                    </div>
-                </div>
-                <div class="answer">
-                    <div class="answer-title">
-                        <h3>Answer 4</h3>
-                    </div>
-                    <div class="answer-content">
-                        <p>Washington D.C.</p>
-                    </div>
-                </div>
-            </div>
+
+            <div class="swiper-pagination"></div>
+
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+            <div class="swiper-scrollbar"></div>
         </div>
+
     </div>
 </div>
+
+
+
+{{-- 
+<div class="quiz-block">
+                        <div class="question">
+                            <div class="question-title">
+                                <h3>Question 1</h3>
+                            </div>
+                            <div class="question-content">
+                                <p>What is the capital of the United States of America?</p>
+                            </div>
+                        </div>
+                        <div class="answers">
+                            <div class="answer">
+                                <div class="answer-title">
+                                    <h3>Answer 1</h3>
+                                </div>
+                                <div class="answer-content">
+                                    <p>Washington D.C.</p>
+                                </div>
+                            </div>
+                            <div class="answer">
+                                <div class="answer-title">
+                                    <h3>Answer 2</h3>
+                                </div>
+                                <div class="answer-content">
+                                    <p>Washington D.C.</p>
+                                </div>
+                            </div>
+                            <div class="answer">
+                                <div class="answer-title">
+                                    <h3>Answer 3</h3>
+                                </div>
+                                <div class="answer-content">
+                                    <p>Washington D.C.</p>
+                                </div>
+                            </div>
+                            <div class="answer">
+                                <div class="answer-title">
+                                    <h3>Answer 4</h3>
+                                </div>
+                                <div class="answer-content">
+                                    <p>Washington D.C.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div> --}}
--- resources/views/pages/student/quiz/page-quiz.blade.php
+++ resources/views/pages/student/quiz/page-quiz.blade.php
@@ -5,7 +5,12 @@
 @section('style-libraries')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
-@stop
+
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+@stop 
 
 @section('styles')
 @stop
@@ -18,6 +23,28 @@
 @stop
 
 @section('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script>
+        const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+        });
     </script>
 @stop
