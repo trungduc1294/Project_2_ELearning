1a8359b99adb783730a7dbe75cadbe15f9ec56f5
trungduc1294 <>
2024-06-05 11:18:31 +0700

    feature: teacher view list student do exam

--- app/Livewire/TeacherExamStatisticLivewire.php
+++ app/Livewire/TeacherExamStatisticLivewire.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Course;
 use App\Models\CourseStudent;
 use App\Models\Exam;
+use App\Models\ExamScore;
 use App\Models\User;
 use Livewire\Component;
 
@@ -15,15 +16,16 @@ class TeacherExamStatisticLivewire extends Component
     public $exam_id;
     public $exam;
 
-    public $listStudent;
+    public $listStudentSubmitedExam;
+    public $listStudentNotSubmitedExam;
 
     public function fetchData() {
-
+        $this->listStudentSubmitedExam = $this->getListStudentSubmitedExam();
+        $this->listStudentNotSubmitedExam = $this->getListStudentNotSubmitedExam();
     }
     public function mount() {
         $this->course = Course::find($this->course_id);
         $this->exam = Exam::find($this->exam_id);
-        $this->listStudent = $this->getListStudentOfCourse();
         $this->fetchData();
     }
     public function render()
@@ -32,13 +34,43 @@ class TeacherExamStatisticLivewire extends Component
     }
 
     // ============================= CUS FUCNT =============================
-    public function getListStudentOfCourse() {
-        $listStudent = [];
-        $listStudentId = CourseStudent::where('course_id', $this->course_id)->pluck('student_id')->toArray();
-        foreach ($listStudentId as $studentId) {
-            $student = User::find($studentId);
-            $listStudent[] = $student;
+
+    public function getListStudentSubmitedExam() {
+        $exam_score_students = ExamScore::where('exam_id', $this->exam_id)->get();
+        foreach ($exam_score_students as $exam_score) {
+            $student = User::find($exam_score->student_id);
+            $list_student_submited_exam[] = [
+                'student_id' => $student->id,
+                'student_name' => $student->username,
+                'score' => $exam_score->score,
+                'temp_score' => $exam_score->temp_score,
+                'is_submited' => true,
+                'submited_time' => $exam_score->created_at,
+            ];
         }
-        return $listStudent;
+        // dd($list_student_submited_exam[0]['student_name']);
+        return $list_student_submited_exam;
+    }
+
+    public function getListStudentNotSubmitedExam() {
+        // get array student_id, who submited exam
+        $exam_score_students_id = ExamScore::where('exam_id', $this->exam_id)->pluck('student_id')->toArray();
+        // get array student_id, who not submited exam
+        $course_students_id = CourseStudent::where('course_id', $this->course_id)->pluck('student_id')->toArray();
+        $list_student_not_submited_exam_id = array_diff($course_students_id, $exam_score_students_id);
+
+        foreach ($list_student_not_submited_exam_id as $student_id) {
+            $student = User::find($student_id);
+            $list_student_not_submited_exam[] = [
+                'student_id' => $student->id,
+                'student_name' => $student->username,
+                'score' => null,
+                'temp_score' => null,
+                'is_submited' => false,
+                'submited_time' => null,
+            ];
+        }
+        // dd($list_student_not_submited_exam[0]['student_name']);
+        return $list_student_not_submited_exam;
     }
 }
--- resources/views/livewire/teacher/exam/teacher-exam-statistic-livewire.blade.php
+++ resources/views/livewire/teacher/exam/teacher-exam-statistic-livewire.blade.php
@@ -1,46 +1,76 @@
-<div class="statistic_container">
-    <div class="top_deco h-3 w-full bg-purple-400 rounded-t-lg"></div>
-    <div class="title_content p-6">
-        <p class="w-full py-2 border-b border-gray-300 text-2xl text-black">
-            {{$exam->title}}
-        </p>
-        <p class="w-full py-2 outline-none border-b border-gray-300 text-black mt-4">
-            {{$exam->description}}
-        </p>
+<div class="statistic_container max-w-[1000px] mx-auto">
+    {{-- Exam header --}}
+    <div class="exam_title bg-white rounded-lg mb-6">
+        <div class="top_deco h-3 w-full bg-purple-400 rounded-t-lg"></div>
+        <div class="title_content p-6">
+            <p class="w-full py-2 border-b border-gray-300 text-2xl text-black">
+                {{$exam->title}}
+            </p>
+            <p class="w-full py-2 outline-none border-b border-gray-300 text-black mt-4">
+                {{$exam->description}}
+            </p>
 
-        <div class="date_time_picker mt-4 flex gap-4">
-            <div class="date_time_picker_group">
-                <label class="block font-medium text-gray-400">Start Time</label>
-                <span class="w-full outline-none border-b border-gray-300 text-gray-500">{{$exam->start_time}}</span>
+            <div class="date_time_picker mt-4 flex gap-4">
+                <div class="date_time_picker_group">
+                    <label class="block font-medium text-gray-400">Start Time</label>
+                    <span class="w-full outline-none border-b border-gray-300 text-gray-500">{{$exam->start_time}}</span>
+                </div>
+                <div class="date_time_picker_group">
+                    <label class="block font-medium text-gray-400">End Time</label>
+                    <span class="w-full outline-none border-b border-gray-300 text-gray-500">{{$exam->end_time}}</span>
+                </div>
             </div>
-            <div class="date_time_picker_group">
-                <label class="block font-medium text-gray-400">End Time</label>
-                <span class="w-full outline-none border-b border-gray-300 text-gray-500">{{$exam->end_time}}</span>
-            </div>
-        </div>
-
-        <div class="button_comntainer flex gap-4">
-            <a class="submit mt-4" href="{{
-                route('teacher.exam.detail', [
-                    'course_id' => $course_id,
-                    'exam_id' => $exam->id
-                ])
-            }}">
-                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-green-400 transform transition duration-200 ease-in-out">
-                    Xem chi tiết
-                </button>
-            </a>
-
-            <a class="submit mt-4" href="{{
-                route('teacher.exam.statistic', [
-                    'course_id' => $course_id,
-                    'exam_id' => $exam->id
-                ])
-            }}">
-                <button class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-green-400 transform transition duration-200 ease-in-out">
-                    Thống kê bài làm
-                </button>
-            </a>
         </div>
     </div>
+
+    {{-- ListStudent --}}
+    <h1 class="text-2xl font-semibold border-t border-dashed border-white py-4">Danh sách học sinh</h1>
+    <table class="w-full">
+        <thead>
+            <t class="">
+                <th class="border border-gray-300 px-4 py-2">STT</th>
+                <th class="border border-gray-300 px-4 py-2">Tên học sinh</th>
+                <th class="border border-gray-300 px-4 py-2">Đã làm bài kiểm tra</th>
+                <th class="border border-gray-300 px-4 py-2">Điểm tạm chấm</th>
+                <th class="border border-gray-300 px-4 py-2">Điểm thực nhận</th>
+                <th class="border border-gray-300 px-4 py-2">Xem bài chi tiết</th>
+            </t>
+        </thead>
+        <tbody>
+            {{-- submited student --}}
+            @foreach ($listStudentSubmitedExam as $index => $student)
+                <tr class="text-center">
+                    <td class="border border-gray-300 px-4 py-2">{{$loop->index + 1}}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{$student['student_name']}}</td>
+                    <td class="border border-gray-300 px-4 py-2">
+                        <i class="fa-regular fa-circle-check text-xl text-green-400"></i>
+                    </td>
+                    <td class="border border-gray-300 px-4 py-2">{{$student['temp_score']}}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{$student['score'] ?? 'Chưa có'}}</td>
+                    <td class="border border-gray-300 px-4 py-2">
+                        <a>
+                            <button class="bg-blue-500 text-white px-4 py-1 rounded-lg hover:bg-green-400 transform transition duration-200 ease-in-out">
+                                Xem chi tiết
+                            </button>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+                
+            {{-- not submited student --}}
+            @foreach ($listStudentNotSubmitedExam as $index => $student)
+                <tr class="text-center">
+                    <td class="border border-gray-300 px-4 py-2">{{$loop->index + 1}}</td>
+                    <td class="border border-gray-300 px-4 py-2">{{$student['student_name']}}</td>
+                    <td class="border border-gray-300 px-4 py-2">
+                        <i class="fa-regular fa-circle-xmark text-xl text-red-400"></i>
+                    </td>
+                    <td class="border border-gray-300 px-4 py-2">Chưa có</td>
+                    <td class="border border-gray-300 px-4 py-2">Chưa có</td>
+                    <td class="border border-gray-300 px-4 py-2">
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
