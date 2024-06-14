f54cf228bc4b503fc6095e5a709ce79ccaf8189c
trungduc1294 <>
2024-06-14 13:11:37 +0700

    update bug change account on route

--- app/Http/Controllers/Account.php
+++ app/Http/Controllers/Account.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class Account extends Controller
 {
     public function index(Request $request) {
+        if((int)session("userId") !== (int)$request->id) {
+            abort(403);
+        }
         $id = $request->id;
         return view('pages.both.page-account', ['user_id' => $id]);
     }
